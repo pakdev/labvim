f74773b76438375a1c59d29969c12933fc50903a
Peter Kurlak <pkurlak@gmail.com>
2017-08-04 07:59:32 -0500

    Make grid size of window and update colors

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from kivy.uix.scatterlayout import ScatterLayout
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.properties import BoundedNumericProperty
-from kivy.graphics import Line
+from kivy.graphics import Line, Color
 from kivy.app import App
 
 
-class Controller(ScatterLayout):
-    pass
+class Main(ScatterLayout):
+    Window.clearcolor = (.25, .25, .25, 1)
 
 
 class Grid(Widget):
@@ -17,16 +17,15 @@ class Grid(Widget):
     def __init__(self, **kwargs):
         super(Grid, self).__init__(**kwargs)
 
-        for i in [-1, 1, 2]:
-            for j in range(int(Window.width / self.grid_width)):
-                x = i * j * self.grid_width
-                grid_line = GridLine(x, -Window.height, x, Window.height * 2)
-                self.add_widget(grid_line)
+        for i in range(int(Window.width / self.grid_width) + 1):
+            x = i * self.grid_width
+            grid_line = GridLine(x, 0, x, Window.height)
+            self.add_widget(grid_line)
 
-            for j in range(int(Window.height / self.grid_height)):
-                y = i * j * self.grid_height
-                grid_line = GridLine(-Window.width, y, Window.width * 2, y)
-                self.add_widget(grid_line)
+        for i in range(int(Window.height / self.grid_height) + 1):
+            y = i * self.grid_height
+            grid_line = GridLine(0, y, Window.width, y)
+            self.add_widget(grid_line)
 
 
 class GridLine(Widget):
@@ -34,12 +33,13 @@ class GridLine(Widget):
         super(GridLine, self).__init__(**kwargs)
 
         with self.canvas:
+            Color(0.5, 0.5, 0.5, 1)
             Line(points=[x1, y1, x2, y2])
 
 
 class LabVIMApp(App):
     def build(self):
-        return Controller()
+        return Main()
 
 
 if __name__ == '__main__':
