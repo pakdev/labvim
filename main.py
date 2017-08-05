65fb966f6609abef727d116f06a722877c81d647
Peter Kurlak <pkurlak@gmail.com>
2017-08-05 15:27:54 -0500

    Make grid fit within window, add cursor, start keyboard

--- main.py
+++ main.py
@@ -1,30 +1,70 @@
+import math
+import atexit
+
 from kivy.uix.scatterlayout import ScatterLayout
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.properties import BoundedNumericProperty
-from kivy.graphics import Line, Color
+from kivy.properties import NumericProperty
+from kivy.graphics import Color
+from kivy.graphics import Line
 from kivy.app import App
 
 
 class Main(ScatterLayout):
     Window.clearcolor = (.25, .25, .25, 1)
 
+    def __init__(self, **kwargs):
+        super(Main, self).__init__(**kwargs)
+
+        self._keyboard = Window.request_keyboard(
+            self._keyboard_closed, self, 'text')
+
+        if self._keyboard.widget:
+            pass
+        self._keyboard.bind(on_key_down=self._on_key_down)
+
+    def cleanup(self):
+        self._keyboard.release()
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_key_down)
+        self._keyboard = None
+
+    def _on_key_down(self, keyboard, keycode, text, modifiers):
+        print(keycode)
+        print(text)
+        print(modifiers)
+
+        # Add logic to handle recognized keys and return True
+        return False
+
 
 class Grid(Widget):
-    grid_width = BoundedNumericProperty(100, min=100, max=500)
-    grid_height = BoundedNumericProperty(100, min=100, max=500)
+    rows = NumericProperty(0)
+    columns = NumericProperty(0)
+    offset = NumericProperty(10)
+
+    cell_width = BoundedNumericProperty(100, min=100, max=500)
+    cell_height = BoundedNumericProperty(100, min=100, max=500)
 
     def __init__(self, **kwargs):
         super(Grid, self).__init__(**kwargs)
 
-        for i in range(int(Window.width / self.grid_width) + 1):
-            x = i * self.grid_width
-            grid_line = GridLine(x, 0, x, Window.height)
+        available_height = Window.height - self.offset
+        available_width = Window.width - self.offset
+
+        self.rows = math.floor(available_height / self.cell_height)
+        self.columns = math.floor(available_width / self.cell_width)
+
+        for i in range(self.rows + 1):
+            y = i * self.cell_height + self.offset
+            grid_line = GridLine(self.offset, y, self.columns * self.cell_width + self.offset, y)
             self.add_widget(grid_line)
 
-        for i in range(int(Window.height / self.grid_height) + 1):
-            y = i * self.grid_height
-            grid_line = GridLine(0, y, Window.width, y)
+        for i in range(self.columns + 1):
+            x = i * self.cell_width + self.offset
+            grid_line = GridLine(x, self.offset, x, self.rows * self.cell_height + self.offset)
             self.add_widget(grid_line)
 
 
@@ -37,9 +77,18 @@ class GridLine(Widget):
             Line(points=[x1, y1, x2, y2])
 
 
+class Cursor(Widget):
+    thickness = BoundedNumericProperty(2, min=1, max=5)
+
+    def __init__(self, **kwargs):
+        super(Cursor, self).__init__(**kwargs)
+
+
 class LabVIMApp(App):
     def build(self):
-        return Main()
+        main = Main()
+        atexit.register(main.cleanup)
+        return main
 
 
 if __name__ == '__main__':
